639316ac67c0200fa8178cddc8e80f01ff6efeff
unknown <zhou7cathy@gmail.com>
2022-12-15 21:38:42 +0800

    simplyfy function in js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ $(function () {
     var description = this.parentElement.children[1].value ;
     localStorage.setItem(this.parentElement.id, description);
   });
-    
 
   //Add code to apply the past, present, or future class to each time
   var currentHour = dayjs().get('hour');
@@ -25,15 +24,12 @@ $(function () {
   }
 
   //Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements.
-  function renderDescription(){
-    for(var i = 9; i <= 17; i++){
-      if(localStorage != null) {
-        $('#hour-' + i).children()[1].value = localStorage.getItem('hour-'+ i);
-      }
+  for(var i = 9; i <= 17; i++){
+    if(localStorage != null) {
+      $('#hour-' + i).children()[1].value = localStorage.getItem('hour-'+ i);
     }
   }
-  
-  renderDescription();
+
 });
 
 
